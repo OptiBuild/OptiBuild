db8641e6a91c915715715359d60bdad82cf9f778
山月 <103094937+Shanyue921@users.noreply.github.com>
2024-03-20 21:37:07 +0800

    Create project structure
    Spring Boot

---
+++ website/src/main/java/com/optibuild/website/OptiBuildWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.optibuild.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptiBuildWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptiBuildWebsiteApplication.class, args);
+	}
+
+}
---
+++ website/src/test/java/com/optibuild/website/OptiBuildWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optibuild.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptiBuildWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
