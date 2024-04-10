25fe58b69951f80b8e99c4d71e5b4a3de5945d8f
albertdagon <albertdagon@gmail.com>
2024-04-10 20:14:18 +0800

    Complete Hardware Components Database
    Will have documentation and images soon.

--- Hardware_Components.sql
+++ Hardware_Components.sql
@@ -16,6 +16,67 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `ComputerCase`
+--
+
+DROP TABLE IF EXISTS `ComputerCase`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ComputerCase` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `brand` varchar(255) DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `driveBays35` int DEFAULT NULL,
+  `driveBays25` int DEFAULT NULL,
+  `maxGPULength` int DEFAULT NULL,
+  `maxCPUCoolerHeight` int DEFAULT NULL,
+  `maxPSULength` int DEFAULT NULL,
+  `dimension` varchar(255) DEFAULT NULL,
+  `weight` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ComputerCase`
+--
+
+LOCK TABLES `ComputerCase` WRITE;
+/*!40000 ALTER TABLE `ComputerCase` DISABLE KEYS */;
+INSERT INTO `ComputerCase` VALUES (1,'Phanteks','Enthoo PRO 2','ATX Full Tower',149.99,12,11,503,195,254,'22.83\" x 9.45\" x 22.05\"',28.7),(2,'Corsair','4000D Airflow','ATX Mid Tower',104.99,2,2,360,170,180,'18.35\" x 9.06\" x 17.83\"',17.31),(3,'Fractal Design','Focus G Mini','m-ATX',54.99,2,1,380,165,230,'15.07\" x 8.07\" x 18.27\"',9.04),(4,'Thermaltake','Tower 100 Black Edition','ITX',89.99,2,2,330,190,180,'18.2\" x 10.5\" x 10.5\"',13.45);
+/*!40000 ALTER TABLE `ComputerCase` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ComputerCase_FormFactorCompatibility`
+--
+
+DROP TABLE IF EXISTS `ComputerCase_FormFactorCompatibility`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ComputerCase_FormFactorCompatibility` (
+  `case_id` bigint NOT NULL,
+  `formFactorType_id` bigint NOT NULL,
+  PRIMARY KEY (`case_id`,`formFactorType_id`),
+  KEY `formFactorType_id` (`formFactorType_id`),
+  CONSTRAINT `computercase_formfactorcompatibility_ibfk_1` FOREIGN KEY (`case_id`) REFERENCES `ComputerCase` (`id`),
+  CONSTRAINT `computercase_formfactorcompatibility_ibfk_2` FOREIGN KEY (`formFactorType_id`) REFERENCES `formFactorCompatibility` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ComputerCase_FormFactorCompatibility`
+--
+
+LOCK TABLES `ComputerCase_FormFactorCompatibility` WRITE;
+/*!40000 ALTER TABLE `ComputerCase_FormFactorCompatibility` DISABLE KEYS */;
+INSERT INTO `ComputerCase_FormFactorCompatibility` VALUES (1,1),(2,1),(1,2),(2,2),(3,2),(1,4),(2,4),(3,4),(4,4),(1,5),(2,5),(1,6);
+/*!40000 ALTER TABLE `ComputerCase_FormFactorCompatibility` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `CPU`
 --
 
@@ -62,8 +123,11 @@ CREATE TABLE `CPUCooler` (
   `height` int DEFAULT NULL,
   `RGB` tinyint(1) DEFAULT NULL,
   `addressable` tinyint(1) DEFAULT NULL,
+  `brand` varchar(255) DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,33 +136,59 @@ CREATE TABLE `CPUCooler` (
 
 LOCK TABLES `CPUCooler` WRITE;
 /*!40000 ALTER TABLE `CPUCooler` DISABLE KEYS */;
+INSERT INTO `CPUCooler` VALUES (1,120,160,0,0,'Deepcool','AK620\r',64.99),(2,120,160,0,0,'Deepcool','AK620\r',64.99),(3,120,168,1,1,'be quiet!','Dark Rock Pro 5\r',99.90),(4,120,168,1,1,'be quiet!','Dark Rock Pro 6\r',99.90),(5,120,148,1,1,'Vetroo','V5\r',24.99),(6,120,148,1,1,'Vetroo','V6\r',24.99),(7,120,155,1,0,'Thermaltake','UX200 Special Edition\r',29.99),(8,120,155,1,0,'Thermaltake','UX200 Special Edition',29.99);
 /*!40000 ALTER TABLE `CPUCooler` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `CPUCooler_SocketCompatiblity`
+-- Table structure for table `CPUCooler_SocketCompatibility`
 --
 
-DROP TABLE IF EXISTS `CPUCooler_SocketCompatiblity`;
+DROP TABLE IF EXISTS `CPUCooler_SocketCompatibility`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `CPUCooler_SocketCompatiblity` (
+CREATE TABLE `CPUCooler_SocketCompatibility` (
   `cooler_id` bigint NOT NULL,
   `compatibility_id` bigint NOT NULL,
   PRIMARY KEY (`cooler_id`,`compatibility_id`),
   KEY `compatibility_id` (`compatibility_id`),
-  CONSTRAINT `cpucooler_socketcompatiblity_ibfk_1` FOREIGN KEY (`cooler_id`) REFERENCES `CPUCooler` (`id`),
-  CONSTRAINT `cpucooler_socketcompatiblity_ibfk_2` FOREIGN KEY (`compatibility_id`) REFERENCES `SocketCompatibility` (`id`)
+  CONSTRAINT `cpucooler_socketcompatibility_ibfk_1` FOREIGN KEY (`cooler_id`) REFERENCES `CPUCooler` (`id`),
+  CONSTRAINT `cpucooler_socketcompatibility_ibfk_2` FOREIGN KEY (`compatibility_id`) REFERENCES `SocketCompatibility` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `CPUCooler_SocketCompatiblity`
+-- Dumping data for table `CPUCooler_SocketCompatibility`
 --
 
-LOCK TABLES `CPUCooler_SocketCompatiblity` WRITE;
-/*!40000 ALTER TABLE `CPUCooler_SocketCompatiblity` DISABLE KEYS */;
-/*!40000 ALTER TABLE `CPUCooler_SocketCompatiblity` ENABLE KEYS */;
+LOCK TABLES `CPUCooler_SocketCompatibility` WRITE;
+/*!40000 ALTER TABLE `CPUCooler_SocketCompatibility` DISABLE KEYS */;
+INSERT INTO `CPUCooler_SocketCompatibility` VALUES (1,1),(1,2),(1,3),(1,4),(4,4),(5,4),(7,4),(1,5),(4,5),(5,5),(7,5),(1,6),(4,6),(1,7),(4,7),(1,8),(4,8),(2,9),(3,9),(6,9),(8,9),(3,10),(6,10),(8,10),(4,11),(5,12),(7,13),(8,14),(8,15),(8,16),(8,17),(8,18),(8,19);
+/*!40000 ALTER TABLE `CPUCooler_SocketCompatibility` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `FormFactorCompatibility`
+--
+
+DROP TABLE IF EXISTS `FormFactorCompatibility`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `FormFactorCompatibility` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `formFactorType` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `FormFactorCompatibility`
+--
+
+LOCK TABLES `FormFactorCompatibility` WRITE;
+/*!40000 ALTER TABLE `FormFactorCompatibility` DISABLE KEYS */;
+INSERT INTO `FormFactorCompatibility` VALUES (1,'ATX\r'),(2,'Micro ATX\r'),(3,'ITX\r'),(4,'Mini ITX\r'),(5,'E-ATX\r'),(6,'SSI-EEB');
+/*!40000 ALTER TABLE `FormFactorCompatibility` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -212,9 +302,9 @@ CREATE TABLE `HDD` (
   `maxTransferRate` int DEFAULT NULL,
   `cache` int DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `hardDriveId` (`hardDriveId`),
+  KEY `fk_hardDriveId` (`hardDriveId`),
   CONSTRAINT `hdd_ibfk_1` FOREIGN KEY (`hardDriveId`) REFERENCES `HardDrives` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -223,7 +313,105 @@ CREATE TABLE `HDD` (
 
 LOCK TABLES `HDD` WRITE;
 /*!40000 ALTER TABLE `HDD` DISABLE KEYS */;
+INSERT INTO `HDD` VALUES (1,94,260,256),(2,95,263,256),(3,96,185,128),(4,97,0,256),(5,98,0,512),(6,99,0,512);
 /*!40000 ALTER TABLE `HDD` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Motherboard`
+--
+
+DROP TABLE IF EXISTS `Motherboard`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Motherboard` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `brand` varchar(255) DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `socket` varchar(255) DEFAULT NULL,
+  `chipset` varchar(255) DEFAULT NULL,
+  `memorySlot` int DEFAULT NULL,
+  `clockSpeed` int DEFAULT NULL,
+  `maxMemory` int DEFAULT NULL,
+  `DDRVersion` int DEFAULT NULL,
+  `PCIe4x16Slots` int DEFAULT NULL,
+  `SATASlots` int DEFAULT NULL,
+  `SATASpeed` int DEFAULT NULL,
+  `M2Slots` int DEFAULT NULL,
+  `M2Type` varchar(255) DEFAULT NULL,
+  `supportNVMe` tinyint(1) DEFAULT NULL,
+  `formFactor` varchar(255) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `CHK_supportNVMe` CHECK ((`supportNVMe` in (0,1)))
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Motherboard`
+--
+
+LOCK TABLES `Motherboard` WRITE;
+/*!40000 ALTER TABLE `Motherboard` DISABLE KEYS */;
+INSERT INTO `Motherboard` VALUES (1,'ASUS','TUF Gaming B650-E WIFI','AMD AM5','AMD B650',4,5200,192,5,2,4,6,3,'(supports PCIe 4.0 x4 mode)',1,'ATX\r',199.99),(2,'ASUS','ROG STRIX B650-A Gaming WIFI 6E','AMD AM5','AMD B650',4,6400,128,5,2,4,6,3,'(supports PCIe 4.0 x4 mode)',1,'ATX\r',259.99),(3,'MSI','GAMING PLUS WIFI','AMD AM5','AMD B650',4,6000,192,5,2,4,6,2,'',1,'ATX\r',169.99),(4,'ASRock','B650M Pro RS WIFI','AMD AM5','AMD B650',4,6000,128,5,2,4,6,3,'',1,'Micro ATX\r',149.99),(5,'Gigabyte','X670E AORUS PRO X','AMD AM5','AMD X670',4,8000,192,5,3,4,6,4,'',1,'ATX\r',299.99),(6,'ASUS','PRIME B450M-A II','AMD AM4','AMD B450',4,4400,128,4,1,6,6,2,'',1,'Micro ATX\r',71.99),(7,'MSI','MAG Z790 TOMAHAWK MAX WIFI','Intel LGA 1700','Intel Z790',4,7800,192,5,2,8,6,4,'',1,'ATX\r',279.99),(8,'ASRock','Z690 PG VELOCITA','Intel LGA 1700','Intel Z690',4,6400,128,5,3,6,6,4,'',1,'ATX\r',149.99),(9,'MSI','B760 GAMING PLUS WIFI','Intel LGA 1700','Intel B760',4,6800,192,5,2,4,6,2,'',1,'ATX\r',159.99),(10,'Gigabyte','H610I','Intel LGA 1700','Intel H610',2,5600,64,5,1,4,6,1,'',0,'ITX',109.99);
+/*!40000 ALTER TABLE `Motherboard` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Motherboard_FormFactorCompatibility`
+--
+
+DROP TABLE IF EXISTS `Motherboard_FormFactorCompatibility`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Motherboard_FormFactorCompatibility` (
+  `motherboard_id` bigint NOT NULL,
+  `formFactorType_id` bigint NOT NULL,
+  PRIMARY KEY (`motherboard_id`,`formFactorType_id`),
+  KEY `formFactorType_id` (`formFactorType_id`),
+  CONSTRAINT `motherboard_formfactorcompatibility_ibfk_1` FOREIGN KEY (`motherboard_id`) REFERENCES `Motherboard` (`id`),
+  CONSTRAINT `motherboard_formfactorcompatibility_ibfk_2` FOREIGN KEY (`formFactorType_id`) REFERENCES `formFactorCompatibility` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Motherboard_FormFactorCompatibility`
+--
+
+LOCK TABLES `Motherboard_FormFactorCompatibility` WRITE;
+/*!40000 ALTER TABLE `Motherboard_FormFactorCompatibility` DISABLE KEYS */;
+INSERT INTO `Motherboard_FormFactorCompatibility` VALUES (1,1),(2,1),(3,1),(5,1),(7,1),(8,1),(9,1),(4,2),(6,2),(10,3);
+/*!40000 ALTER TABLE `Motherboard_FormFactorCompatibility` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `PowerSupply`
+--
+
+DROP TABLE IF EXISTS `PowerSupply`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PowerSupply` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `model` varchar(255) DEFAULT NULL,
+  `brand` varchar(255) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `formFactor` varchar(255) DEFAULT NULL,
+  `maxPower` int DEFAULT NULL,
+  `maxPSULength` int DEFAULT NULL,
+  `modular` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `PowerSupply`
+--
+
+LOCK TABLES `PowerSupply` WRITE;
+/*!40000 ALTER TABLE `PowerSupply` DISABLE KEYS */;
+INSERT INTO `PowerSupply` VALUES (1,'RM750e','Corsair',89.99,'ATX',750,151,'Yes\r'),(2,'RM1000e','Corsair',139.99,'ATX',1000,151,'Yes\r'),(3,'XP1000W 80PLUS PLATINUM','Sama',129.99,'ATX',1000,140,'Yes\r'),(4,'Hybrid II Series','Raidmax',49.99,'ATX',530,159,'Semi-Modular\r'),(5,'SuperNOVA 750 GM','EVGA',149.99,'SFX',750,100,'Yes\r'),(6,'SFX Series','SilverStone',65.99,'SFX',300,100,'No\r'),(7,'650 N1','EVGA',59.99,'ATX',650,140,'No\r'),(8,'','',0.00,'',0,0,NULL);
+/*!40000 ALTER TABLE `PowerSupply` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -269,7 +457,7 @@ CREATE TABLE `SocketCompatibility` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `socketType` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -278,6 +466,7 @@ CREATE TABLE `SocketCompatibility` (
 
 LOCK TABLES `SocketCompatibility` WRITE;
 /*!40000 ALTER TABLE `SocketCompatibility` DISABLE KEYS */;
+INSERT INTO `SocketCompatibility` VALUES (1,'Intel LGA 2066'),(2,'Intel LGA 2011-3'),(3,'Intel LGA 2011'),(4,'Intel LGA 1700'),(5,'Intel LGA 1200'),(6,'Intel LGA 1155'),(7,'Intel LGA 1151'),(8,'Intel LGA 1150'),(9,'AMD AM4'),(10,'AMD AM5'),(11,'Intel LGA 1156'),(12,'Intel LGA 115X'),(13,'Intel LGA 115x'),(14,'AMD AM3+'),(15,'AMD AM3'),(16,'AMD AM2+'),(17,'AMD AM2'),(18,'AMD FM2'),(19,'AMD FM1');
 /*!40000 ALTER TABLE `SocketCompatibility` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -297,7 +486,7 @@ CREATE TABLE `SSD` (
   PRIMARY KEY (`id`),
   KEY `hardDriveId` (`hardDriveId`),
   CONSTRAINT `ssd_ibfk_1` FOREIGN KEY (`hardDriveId`) REFERENCES `HardDrives` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -306,6 +495,7 @@ CREATE TABLE `SSD` (
 
 LOCK TABLES `SSD` WRITE;
 /*!40000 ALTER TABLE `SSD` DISABLE KEYS */;
+INSERT INTO `SSD` VALUES (1,100,7300,6300,'3D NAND TLC'),(2,101,5000,4200,'V-NAND TLC'),(3,102,550,500,'3D NAND'),(4,103,540,490,'3D TLC'),(5,104,3500,3000,'V-NAND MLC');
 /*!40000 ALTER TABLE `SSD` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -318,4 +508,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-04  4:51:32
+-- Dump completed on 2024-04-10 20:09:43
